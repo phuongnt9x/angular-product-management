efc52b217b2321742cc63c60e1d326f730781f42
Nguyen Phuong <95663567+phuongnt9x@users.noreply.github.com>
2021-12-16 10:37:07 +0700

    ex-product-management-moudule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductCreateComponent } from './product/product-create/product-create.component';
-import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
-import {ProductUpdateComponent} from "./product/product-update/product-update.component";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductCreateComponent,
-    ProductUpdateComponent,
-    ProductDeleteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
